da8f3164530e6109a51e556d28e84fa7898a4b84
Ivan Povalyukhin <ivanpoval@gmail.com>
2014-12-30 16:28:12 -0800

    comment cors whitelist

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -2,8 +2,8 @@ class ProjectsController < ApplicationController
   def index
     projects = ['Gobller', 'Machinima']
 
-    response.header["Access-Control-Allow-Origin"] = "*"
-    response.header["Access-Control-Allow-Headers"] = "Origin, X-Requested-With, Content-Type, Accept"
+#    response.header["Access-Control-Allow-Origin"] = "*"
+#    response.header["Access-Control-Allow-Headers"] = "Origin, X-Requested-With, Content-Type, Accept"
 
     render json: projects
   end
