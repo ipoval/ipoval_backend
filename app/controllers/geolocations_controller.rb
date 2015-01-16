c2936638c74d90e7075a0b0c5059294cfce2ad8b
Ivan Povalyukhin <ivanpoval@gmail.com>
2015-01-15 18:33:43 -0800

    added endpoint to support geolocation search for website

--- app/controllers/geolocations_controller.rb
+++ app/controllers/geolocations_controller.rb
@@ -3,6 +3,22 @@ class GeolocationsController < ApplicationController
     response.header["Access-Control-Allow-Origin"] = "*"
     response.header["Access-Control-Allow-Headers"] = "Origin, X-Requested-With, Content-Type, Accept"
 
-    render(json: { location: 'Irvine, CA' })
+    location = Geocoder.search(params.slice(:latitude, :longitude).values.join(',')).first
+    location_str = ''
+
+    city = location.data["address_components"].detect { |tuple| tuple['types'].include?('locality') }
+    if city.present?
+      location_str << city['long_name']
+    end
+
+    state = location.data["address_components"].detect { |tuple| tuple['types'].include?('administrative_area_level_1') }
+    if state.present?
+      location_str << ', ' if city.present?
+      location_str << state['short_name']
+    end
+
+    render(json: { location: location_str })
+  rescue
+    render(json: { location: '' })
   end
 end
