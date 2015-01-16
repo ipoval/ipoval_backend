0e813b32d7e1bd5c09f9dfffb7159946e53d16c1
Ivan Povalyukhin <ivanpoval@gmail.com>
2015-01-15 17:12:04 -0800

    added very open CORS headers

--- app/controllers/geolocations_controller.rb
+++ app/controllers/geolocations_controller.rb
@@ -1,5 +1,8 @@
 class GeolocationsController < ApplicationController
   def current
+    response.header["Access-Control-Allow-Origin"] = "*"
+    response.header["Access-Control-Allow-Headers"] = "Origin, X-Requested-With, Content-Type, Accept"
+
     render(json: { location: 'Irvine, CA' })
   end
 end
