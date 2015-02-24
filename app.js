e43281b52a1e56ed1bff8c20921a3ae879fc63d2
Ivan Povalyukhin <ivanpoval@gmail.com>
2015-02-23 18:41:31 -0800

    rename heroku button file

--- app.js
+++
@@ -1,7 +0,0 @@
-{
-  "name": "iPoval backend APIs",
-  "description": "iPoval backend APIs",
-  "repository": "https://github.com/ipoval/ipoval_backend",
-  "logo": "https://node-js-sample.herokuapp.com/node.svg",
-  "keywords": ["ruby", "rails", "ipoval"]
-}
