d40f7a48ff62265a1589a933afb048e175c9291f
Ivan Povalyukhin <ivanpoval@gmail.com>
2015-03-17 19:00:02 -0700

    added html response for \/ uri

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'application#index'
 
   resources :projects, only: [:index], format: 'json'
   resources :geolocations, only: [:show] do
